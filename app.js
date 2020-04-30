4ba51ddc0bc4f7acc598c613494e1dcd7cbc6643
Elle <ekoshiro1@gmail.com>
2020-04-30 08:51:39 -0700

    first real commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const morgan = require("morgan"); 
+const app = express(); 
+const layout = require("./views/layout"); 
+
+
+app.use(morgan("dev")); 
+
+app.use(express.static(__dirname + "/public")); 
+
+app.use(express.urlencoded({extended: false})); 
+
+app.get('/', (req, res, next) => {
+    console.log('hello world!'); 
+    res.send(layout('')); 
+});
+
+const PORT = 3000; 
+
+app.listen(PORT, () => {console.log('Listening in port 3000')})
